07156f6ade24719570be41bc6ff65e6d6f733cc2
Jun7892 <sh98022000@gmail.com>
2022-02-23 20:36:55 +0900

    second page 모달창 bgm 추가

--- static/js/second.js
+++ static/js/second.js
@@ -18,3 +18,28 @@ function charm2(sound2) {
     audio.play();
     console.log('aaaa');
 }
+
+
+
+function charm3(sound3) {
+    let audio = new Audio(sound3);
+    audio.volume = 0.5;
+    audio.play();
+    console.log('aaaa');
+
+}
+
+// 버튼 클릭시 display: flex 값 / 닫기 버튼 클릭시 modal display none 값
+
+// const modal = document.getElementById("modal")
+
+function modalOn() {
+    const modal = document.getElementById("modal")
+    modal.style.display = "flex"
+}
+
+function modalOff() {
+    const modal = document.getElementById("modal")
+    console.log(modal)
+    modal.style.display = "none"
+}
